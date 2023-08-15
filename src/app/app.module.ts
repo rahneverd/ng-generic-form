7096f167bf2e2d0aa3e1663fab125944dd2bea73
Muhammad Owais <rahneverd@gmail.com>
2023-08-15 10:35:22 +0500

    created generic form component and data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GenericFormComponent } from './components/generic-form/generic-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenericFormComponent
   ],
   imports: [
     BrowserModule,
