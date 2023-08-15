67eb968c98ce7558a3788177a4784fb934ffc194
Muhammad Owais <rahneverd@gmail.com>
2023-08-15 10:50:54 +0500

    updated data service and using app-generic-component inside app component

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  jsonFormData = new BehaviorSubject({
+    controls: [
+      {
+        name: 'firstName',
+        label: 'First name:',
+        value: '',
+        type: 'text',
+        validators: {
+          required: true,
+          minLength: 10,
+        },
+      },
+      {
+        name: 'lastName',
+        label: 'Last name:',
+        value: '',
+        type: 'text',
+        validators: {},
+      },
+      {
+        name: 'comments',
+        label: 'Comments',
+        value: '',
+        type: 'textarea',
+        validators: {},
+      },
+      {
+        name: 'agreeTerms',
+        label: 'This is a checkbox?',
+        value: 'false',
+        type: 'checkbox',
+        validators: {},
+      },
+      {
+        name: 'size',
+        label: '',
+        value: '',
+        type: 'range',
+        options: {
+          min: '0',
+          max: '100',
+          step: '1',
+          icon: 'sunny',
+        },
+        validators: {},
+      },
+      {
+        name: 'lightDark',
+        label: 'Do you like toggles?',
+        value: 'false',
+        type: 'toggle',
+        validators: {},
+      },
+    ],
+  });
+  jsonFormData$ = this.jsonFormData.asObservable();
 
-  constructor() { }
+  constructor() {}
+
+  setJsonFormData(jsonFormData: any) {
+    this.jsonFormData.next(jsonFormData);
+  }
 }
